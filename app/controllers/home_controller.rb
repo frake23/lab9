c369722bd786fcd59ca5aadbfaec3b14939f038d
Dmitry <dimsacom@gmail.com>
2021-12-07 13:13:41 +0300

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,37 +1,17 @@
 # frozen_string_literal: true
 
+require 'sequence'
+
 # home controller
 class HomeController < ApplicationController
   def calc
     arr_str = params[:arr]
     if /^(-?\d+ ?)+$/.match? arr_str
       @arr = arr_str.split(' ').map(&:to_i)
-      @distances = find_distances
+      @distances = Sequence.find_distances @arr
       @max_distance = @distances.max { |a, b| a.length <=> b.length }
     else
-      @error = 'В строке могут быть только числа или пробелы / может быть не более одного пробела между числами'
+      flash.alert = 'В строке могут быть только числа или пробелы / может быть не более одного пробела между числами'
     end
   end
-
-  def find_distances
-    @arr
-      .map { |i| po5?(i) ? i : 0 }
-      .reduce([0, []]) { |obj, i| i.zero? ? [obj[0] + 1, obj[1]] : [obj[0], obj[1].push([obj[0], i])] }[1]
-      .group_by { |i| i[0] }
-      .values
-      .map { |i| i.map { |j| j[1] } }
-  end
-
-  def po5?(num)
-    return false if num <= 0
-
-    while num != 1
-      return false unless (num % 5).zero?
-
-      num /= 5
-    end
-    true
-  end
-
-  helper_method :find_distances, :po5?
 end
---
+++ lib/sequence.rb
@@ -0,0 +1,24 @@
+module Sequence
+  def find_distances(arr)
+    arr
+      .map { |i| po5?(i) ? i : 0 }
+      .slice_when { |i, j| po5?(i) != po5?(j) }
+      .to_a
+      .reject { |i| i.include? 0 }
+  end
+
+  def po5?(num)
+    return false if num <= 0
+
+    while num != 1
+      return false unless (num % 5).zero?
+
+      num /= 5
+    end
+    true
+  end
+
+  private :po5?
+  module_function :find_distances, :po5?
+end
+
