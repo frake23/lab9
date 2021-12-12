2590569afb690ca46a2b5c0ee6ddc75d27beb473
frake23 <dimsacom@gmail.com>
2021-12-13 00:52:09 +0300

    completion

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,34 +1,17 @@
 # frozen_string_literal: true
 
+require 'sequence'
+
 # home controller
 class HomeController < ApplicationController
   def calc
     arr_str = params[:arr]
     if /^(-?\d+ ?)+$/.match? arr_str
       @arr = arr_str.split(' ').map(&:to_i)
-      @distances = find_distances
+      @distances = Sequence.find_distances @arr
       @max_distance = @distances.max { |a, b| a.length <=> b.length }
     else
       @error = 'В строке могут быть только числа или пробелы / может быть не более одного пробела между числами'
     end
-  end
-
-  def find_distances
-    @arr
-      .map {|i| po5?(i) ? i : 0}
-      .slice_when {|i, j| po5?(i) != po5?(j)}
-      .to_a
-      .reject {|i| i.include? 0}
-  end
-
-  def po5?(num)
-    return false if num <= 0
-
-    while num != 1
-      return false unless (num % 5).zero?
-
-      num /= 5
-    end
-    true
   end
 end
